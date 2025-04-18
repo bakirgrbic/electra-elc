e3aae6b4e5b1480f055c9233659c9cda843247ce
Bakir Grbic <97543350+bakirgrbic@users.noreply.github.com>
2025-04-18 15:29:24 -0600

    personal evaluation ready for smoke test

--- evaluation/personal_evaluation/personal_evaluation.py
+++ evaluation/personal_evaluation/personal_evaluation.py
@@ -3,6 +3,8 @@ a personal evaluation method.
 """
 
 import torch
+from sklearn.metrics import accuracy_score
+from transformers import ElectraTokenizerFast
 
 def load_data() -> tuple:
     """Loads and splits evaluation data into train and test."""
@@ -72,26 +74,75 @@ def validation(model, testing_loader, device):
     return torch.stack(fin_outputs), torch.stack(fin_targets)
 
 
-def evaluate(): # TODO: need data, optimizer, model, and likely device
-    for epoch in range(EPOCHS):
-        log(f"Begining Fine Tuning on Epoch {EPOCHS}") # should have been printing epoch and not EPOCHS
-        loss = train(model, training_loader, optimizer)
-        log(f'Epoch: {epoch}, Loss:  {loss.item()}')
-        guess, targs = validation(model, testing_loader)
+def evaluate(model, training_loader, testing_loader, optimizer, device, epochs, logger):
+    """Evaluates a model on a discriminative text topic classification task.
+
+    Keyword Arguments:
+    model -- torch model to validate
+    training_loader -- torch data loader with train data 
+    testing_loader -- torch data loader with test data 
+    optimizer -- torch optimizer
+    device -- which hardware device to use
+    epochs -- the number of epochs to finetune model on
+    logger -- logging.Logger object to log information
+    """
+    for epoch in range(epochs):
+        log(logger, f"Begining Fine Tuning on Epoch {epoch}")
+        loss = train(model, training_loader, optimizer, device)
+        log(logger, f'Epoch: {epoch}, Loss:  {loss.item()}')
+        guess, targs = validation(model, testing_loader, device)
         guesses = torch.argmax(guess, dim=1)
         targets = targs
-        log(f"Epoch {epoch}'s arracy on test set {accuracy_score(guesses, targets)}")
-    log("Fine-tuning Done!")
+        log(logger, f"Epoch {epoch}'s arracy on test set {accuracy_score(guesses, targets)}")
+    log(logger, "Fine-tuning Done!")
 
 
-def main(): # TODO
-    # Define hyperparams and constants
-        # Silent output so its not in terminal?
-        # What about logging? should this func start a new one or simply be passed one?
-    # Get data
-    # Set up datasets and loaders
-    # set up model and tokenizer
-    # call evaluate func
+def personal_evaluation():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    labels = {
+        0:'Computer Science',
+        1:'Electrical Engineering',
+        2:'Psychology',
+        3:'Mechanical Engineering',
+        4:'Civil Engineering',
+        5:'Medical Science',
+        6:'Biochemistry'
+    }
+    MAX_LEN = 128
+    BATCH_SIZE = 64
+    EPOCHS = 3
+    NUM_OUT = len(labels)  # multilabel task
+    LEARNING_RATE = 2e-05
+    MODEL_NAME = "ELECTRA_pt" # TODO: make this and other hyperparams cli inputs
+    TASK_NAME = "personal_evaluation"
+
+    logger = get_my_logger(MODEL_NAME, TASK_NAME)
+    log(logger ,"Background logger started")
+
+    log(logger, "Loading data for personal evaluation fine-tuning")
+    train_X, train_y, test_X, test_y = load_data()
+    training_data = MultiLabelDataset(train_X, torch.from_numpy(train_y), tokenizer, MAX_LEN)
+    testing_data = MultiLabelDataset(test_X, torch.from_numpy(test_y), tokenizer, MAX_LEN)
+    train_params = {'batch_size': BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+    test_params = {'batch_size': BATCH_SIZE,
+                    'shuffle': True,
+                    'num_workers': 0
+                }
+    training_loader = torch.utils.data.DataLoader(training_data, **train_params)
+    testing_loader = torch.utils.data.DataLoader(testing_data, **test_params)
+
+    log(logger, "Loading tokenizer, model and optimizer")
+    tokenizer = ElectraTokenizerFast.from_pretrained("bsu-slim/electra-tiny")
+    model = ELECTRAClass(NUM_OUT)
+    model.to(device)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
+
+    log(logger, f"Using {device} to fine-tune for {EPOCHS} epochs!")
+    evaluate(model, training_loader, testing_loader, optimizer, device, EPOCHS):
+    log(logger, "Fine-tuning Done!")
 
 if __name__ == "__main__":
-    evaluate()
+    main()
