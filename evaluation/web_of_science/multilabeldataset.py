55d9e1c945ffb7cd661bb8a0f95aa01a2babb048
Bakir Grbic <97543350+bakirgrbic@users.noreply.github.com>
2025-05-27 18:02:05 -0600

    multilabledataset typehinted, doc rewording, and refactor

--- evaluation/web_of_science/multilabeldataset.py
+++ evaluation/web_of_science/multilabeldataset.py
@@ -1,48 +1,49 @@
-"""Dataset class used for personal evaluation."""
+"""Dataset class used for web of science evaluation."""
 
 import torch
+import numpy as np
 
 
 class MultiLabelDataset(torch.utils.data.Dataset):
     """Loads and preprocesses the given text data."""
 
-    def __init__(self, text, labels, tokenizer, max_len):
+    def __init__(
+        self, text: list[str], labels: np.ndarray, tokenizer, max_len: int
+    ) -> None:
         """Constructor.
 
         Keyword Arguments:
         text -- text to construct dataset
         labels -- corresponding labels for each text
-        tokenizer -- tokenizer from transformer lib
+        tokenizer -- transformer tokenizer
         max_len -- controls maximum length of tokenizer to truncate or pad input
         """
-        self.tokenizer = tokenizer
         self.text = text
         self.targets = labels
+        self.tokenizer = tokenizer
         self.max_len = max_len
 
     def __len__(self) -> int:
+        """Returns the total number of documents to finetune on."""
         return len(self.text)
 
-    def __getitem__(self, index: int) -> dict:
-        """Tokenizes text at a given index."""
+    def __getitem__(self, index: int) -> dict[str, torch.LongTensor]:
+        """Returns text from specific index with tokenized information."""
         text = self.text[index]
-        inputs = self.tokenizer.encode_plus(
+        inputs = self.tokenizer(
             text,
-            None,
             add_special_tokens=True,
             max_length=self.max_len,
             padding="max_length",
             truncation=True,
-            return_token_type_ids=True
+            return_token_type_ids=False,
         )
 
-        ids = inputs['input_ids']
-        mask = inputs['attention_mask']
-        token_type_ids = inputs["token_type_ids"]
+        ids = inputs["input_ids"]
+        mask = inputs["attention_mask"]
 
         return {
-            'ids': torch.tensor(ids, dtype=torch.long),
-            'mask': torch.tensor(mask, dtype=torch.long),
-            'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
-            'targets': torch.tensor(self.targets[index], dtype=torch.long)
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "targets": self.targets[index].clone().detach(),
         }
