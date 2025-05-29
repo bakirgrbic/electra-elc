295279821c501144b24726ce6692832199d4bf89
Bakir Grbic <97543350+bakirgrbic@users.noreply.github.com>
2025-05-29 01:42:28 -0600

    auto typehinted and doc update

--- evaluation/web_of_science/auto.py
+++ evaluation/web_of_science/auto.py
@@ -7,25 +7,25 @@ from transformers import AutoModel
 class AutoClass(torch.nn.Module):
     """Auto class for finetuning pre-trained models."""
 
-    def __init__(self, model_path, num_out):
+    def __init__(self, model_name: str, num_out: int):
         """Constructor.
 
         Keyword Arguments:
-        model_path -- relative file path of pre-trained model or name from 
+        model_name -- relative file path of pre-trained model or name from
                       huggingface
         num_out -- number of classes to classify
         """
         super().__init__()
-        self.l1 = AutoModel.from_pretrained(model_path)
+        self.l1 = AutoModel.from_pretrained(model_name)
         self.classifier = torch.nn.Linear(196, num_out)
         self.softmax = torch.nn.Softmax(dim=1)
 
-    def forward(self, input_ids, attention_mask):
-        """Runs a piece of data through the model.
+    def forward(self, input_ids: torch.Tensor, attention_mask: torch.Tensor):
+        """Runs a piece of tokenized data through the model.
 
         Keyword Arguments:
-        input_ids -- input_ids for a tokenized line of input
-        attention_mask -- attention mask for input_ids, also from tokenizer
+        input_ids -- input_ids from a transformer tokenizer
+        attention_mask -- attention mask for input_ids from transformer tokenizer
 
         Returns the softmax distribution for all classes
         """
@@ -34,4 +34,5 @@ class AutoClass(torch.nn.Module):
         pooler = hidden_state[:, 0]
         output = self.classifier(pooler)
         output = self.softmax(output)
+
         return output
